19dc8116da6b06fe22afb85920e96faa7a984e79
JuusoKoivuranta <jkoivura20@student.oulu.fi>
2025-07-19 18:03:46 +0300

    Home & About Update

--- KoivuKoodi/app/src/components/front-pages/About.js
+++ KoivuKoodi/app/src/components/front-pages/About.js
@@ -1,15 +1,26 @@
 import React from 'react';
-import './Home.css';
+import { FaGithub } from 'react-icons/fa';
 import './About.css';
 
-const Gallery = () => {
+const About = () => {
     return (
-        <div className='main'>
-            <h1>About Page</h1>
-            <p>A hobby project for learning how to use and program ReactJS :)</p>
-            <p>Made by Juuso Koivuranta.</p>
+        <div className='about-page-container'>
+            <div className='about-main'>
+                <h1>About Page</h1>
+                <p>A hobby project for learning how to use and program ReactJS :)</p>
+                <p>GitHub Copilot Pro was used in this project.</p>
+                <p>Made by Juuso Koivuranta.</p>
+                
+                <a href="https://github.com/JuusoKoivuranta/KoivuKoodiReact" 
+                   target="_blank" 
+                   rel="noopener noreferrer" 
+                   className="github-link">
+                    <FaGithub className="github-logo" />
+                    <span className="github-text">View on GitHub</span>
+                </a>
+            </div>
         </div>
     );
 };
 
-export default Gallery;
+export default About;
--- KoivuKoodi/app/src/components/front-pages/Home.js
+++ KoivuKoodi/app/src/components/front-pages/Home.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
   return (
-    <div className='main'>
-      <h2>Welcome to the KoivuKoodi ReactJS Test Page!</h2>
-      <p>Explore my collection of different react components and how they operate!</p>
+    <div className='home-page-container'>
+      <div className='main'>
+        <h1>KoivuKoodi</h1>
+        <p>Project Library Made by Juuso Koivuranta</p>
+        
+        <div className='grid-container'>
+          <Link to="/gallery" className='grid-item'>
+            <div className='item-content'>Gallery</div>
+          </Link>
+          <Link to="/chess" className='grid-item'>
+            <div className='item-content'>Chess</div>
+          </Link>
+          <Link to="/chatroom" className='grid-item'>
+            <div className='item-content'>Chatroom</div>
+          </Link>
+          <Link to="/minesweeper" className='grid-item'>
+            <div className='item-content'>Minesweeper</div>
+          </Link>
+          <Link to="/about" className='grid-item'>
+            <div className='item-content'>About</div>
+          </Link>
+          <Link to="/coming-soon" className='grid-item'>
+            <div className='item-content'>Coming Soon</div>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
